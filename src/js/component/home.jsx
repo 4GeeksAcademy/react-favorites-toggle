c6980c8b1a1c43e44820b2fcae3967fbf9165b07
Valerie Dubach <116690000+Valerieclaire96@users.noreply.github.com>
2024-09-30 22:31:03 +0000

    finished favorites button

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [favorited, setFavorited] = useState(false)
+
+	const handleFavorites = (e) => {
+		e.preventDefault();
+		if(favorited == true) {
+			setFavorited(false);
+		}
+		else if(favorited == false) {
+			setFavorited(true)
+		}
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="my-5">
+			<div className="card mx-auto" style={{"width": "18rem"}}>
+				<img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcScUqzU0YKUJdmd_eMXHl-k7-1eh0DrLM3Fi0hXjZOm_CmXNAs5ZzpnxYVT1-nrUhZveOtTa1qTvsVdToGuS0oYGg73aZLcH5p9tS_OyA" className="card-img-top" alt="..."/>
+				<div className="card-body">
+					<h5 className="card-title">Cutest Squirt in all the Galaxy</h5>
+					<p className="card-text">**do not leave unattended around frogs</p>
+					<span className={favorited == true ? "glow" : ""}><span className="heart" onClick={(e) => handleFavorites(e)}></span></span>
+				</div>
+			</div>
 		</div>
 	);
 };
